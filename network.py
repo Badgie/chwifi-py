337c6496edad4f60c9303eed7e415d83175075c7
cogitantium <falke.cs@gmail.com>
2019-08-03 19:07:39 +0200

    Add further work on network.py

--- network.py
+++ network.py
@@ -1,42 +1,70 @@
 import netifaces
 import re
 from getpass import getpass
-
 import pexpect
 
+
+# TODO; develop nicer method for executing sudo-commands;
+#  1. preloading sudo for all spawned processes,
+#  2. saving sudo-password during runtime
 
 class Profile:
     state = bool
     profile_name = ""
     work_profile = bool
     adapter = ""
-    old_mac = ""
+    current_mac = ""
     new_mac = ""
 
     def __init__(self, profile_name):
         self.profile_name = profile_name
         self.state = is_profile_active(self.profile_name)
-        self.old_mac = get_mac_address(self.adapter)
+        self.current_mac = get_mac_address(self.adapter)
         # TODO: how to handle setting work profiles? Config?
 
     def connect(self, change_mac):
         if self.state:
-            print("Profile is already active, restarting it now")
+            print("Profile is already active, restarting it now.")
             self.restart()
             return
 
-        if change_mac:
-            # change mac_address
-            pexpect.spawn('sudo macchanger' + self.adapter)
+        # If any profile is active, stop-all before continuing
+        active_profile = is_any_profile_active()
+        if active_profile[0]:
+            print(f"Profile: {active_profile[1]} is currently active. Stopping it before continuing")
+            stop_all_profiles()
 
-        pexpect.spawn('netctl', ['start', self.profile_name])
+        if change_mac:  # todo; handle enabling of macchanger and options through config?
+            change_mac_address(self.adapter, '-e')
+
+        pexpect.run('netctl', ['start', self.profile_name])
         self.state = True
 
+        # todo; wait for connection
+
+        # todo; update cache
+
     def restart(self):
-        pexpect.spawn('netctl', ['restart', self.profile_name])
+        pexpect.run(['sudo', 'netctl', 'restart', self.profile_name])
 
     def disconnect(self):
-        pexpect.spawn('netctl', ['stop', self.profile_name])
+        pexpect.run(['sudo', 'netctl', 'stop', self.profile_name])
+
+
+def stop_all_profiles() -> None:
+    """
+    Stops all profiles through 'netctl'
+    :return: None
+    """
+    child = pexpect.spawn('sudo netctl stop-all', timeout=10)
+    # Wait for sudo-prompt
+    child.expect(re.escape("[sudo] password for ") + r".*:")
+
+    # Use getpass for securely passing password to spawned process
+    child.send(getpass() + "\n")
+
+    # Wait for EOF before continuing connection routine
+    child.expect(pexpect.EOF)
 
 
 def get_mac_address(adapter: str) -> str:
@@ -50,11 +78,12 @@ def get_mac_address(adapter: str) -> str:
 
 def change_mac_address(adapter: str, options: str) -> str:
     try:
-        assert is_any_profile_active() is False
+        assert not is_any_profile_active()[0]  # Ensure that no profile is currently running
     except AssertionError:
-        print("[Error] A profile")
+        print("[Error] A profile is already active. Stopping all profiles and retrying.")
+
     child = pexpect.spawn('sudo macchanger ' + options + ' ' + adapter, timeout=10)
-    child.expect(re.escape("[sudo] password for ") + ".*:")
+    child.expect(re.escape("[sudo] password for ") + r".*:")
 
     # Use getpass for securely passing password to spawned process
     child.send(getpass() + "\n")
@@ -77,12 +106,14 @@ def is_profile_active(profile: str) -> bool:
     (output, exit_code) = pexpect.run('netctl list', withexitstatus=1)
     assert exit_code == 0
     assert output is not None
+    # Returns boolean on whether given profile is prefixed with 'netctl' standard '* ' pattern on listing
     return any(filter(re.compile(re.escape('* ') + profile + r'\s').match, output.decode().split('\n')))
 
 
 def is_any_profile_active() -> (bool, str):
     """
-    Runs 'netctl list' and checks whether any profile is marked '^\*'.
+    Runs 'netctl list' and checks whether any profile is marked '^\*'. If a match is found, the profile name is
+    extracted and returned in the tuple, otherwise (False, '') is returned.
     :return: tuple consisting of boolean on whether any profile is active, and name of active profile
     """
     (output, exit_code) = pexpect.run('netctl list', withexitstatus=1)
